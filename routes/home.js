4474fbe7622c0a36c5aa64ebd3d56542b404e213
rwhite83 <rwhite83@hotmail.com>
2020-01-02 00:21:22 -0800

    added warning to login page

--- routes/home.js
+++ routes/home.js
@@ -13,6 +13,7 @@ router.get('/', function (req, res) {
     }
     else {
         console.log('user logged in.  forwarding to index');
+        // b()
         entry_controller.get_user_entries( function(entry_data) {
             console.log(entry_data);
             res.render('index', { 'entryList': entry_data, 'emailData': cookieSession.userEmail, 'messageData':'welcome to the final countdown.  please press the button' } );
@@ -21,17 +22,3 @@ router.get('/', function (req, res) {
 })
 
 module.exports = router;
-
-/*
-const user_controller = require('../controllers/user_controller');
-const express = require('express');
-const router = express.Router();
-
-router.post('/login', user_controller.login_user);
-
-router.post('/logout', user_controller.logout_user);
-
-router.post('/signup', user_controller.signup_user);
-
-module.exports = router;
-*/
